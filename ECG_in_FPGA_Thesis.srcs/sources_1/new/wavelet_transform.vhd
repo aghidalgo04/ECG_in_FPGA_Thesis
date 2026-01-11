d624354de48db3ac8dc6d878503b4e1188cabef7
aghidalgo04 <a.ghidalgo04@gmail.com>
2026-01-11 18:25:28 +0100

    wavelet_transform definition

--- ECG_in_FPGA_Thesis.srcs/sources_1/new/wavelet_transform.vhd
+++ ECG_in_FPGA_Thesis.srcs/sources_1/new/wavelet_transform.vhd
@@ -1,38 +1,29 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11.01.2026 18:00:57
--- Design Name: 
--- Module Name: wavelet_transform - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity wavelet_transform is
---  Port ( );
+Port (
+        -- 1. CONTROL GENERAL
+        clk             : in  STD_LOGIC; -- Reloj de 100 MHz
+        reset           : in  STD_LOGIC;
+        
+        -- 2. ENTRADA DE DATOS (Vienen del módulo ADS1293)
+        sample_valid_in : in  STD_LOGIC; -- El pulso de "Dato Nuevo"
+        raw_x           : in  STD_LOGIC_VECTOR(23 downto 0);
+        raw_y           : in  STD_LOGIC_VECTOR(23 downto 0);
+        raw_z           : in  STD_LOGIC_VECTOR(23 downto 0);
+
+        -- 3. SALIDAS DE SEÑAL LIMPIA (Para visualizar/CORDIC)
+        -- La Wavelet elimina el ruido de alta frecuencia y la línea base
+        clean_x         : out STD_LOGIC_VECTOR(23 downto 0);
+        clean_y         : out STD_LOGIC_VECTOR(23 downto 0);
+        clean_z         : out STD_LOGIC_VECTOR(23 downto 0);
+        clean_valid     : out STD_LOGIC; -- Pulso cuando el dato limpio está listo
+
+        -- 4. SALIDAS DE ANÁLISIS (Resultados Médicos)
+        qrs_detected    : out STD_LOGIC; -- Un pulso '1' cuando detectas un pico R
+        heart_rate      : out STD_LOGIC_VECTOR(8 downto 0) -- PPM calculados (ej. 60, 120)
+    );
 end wavelet_transform;
 
 architecture Behavioral of wavelet_transform is
