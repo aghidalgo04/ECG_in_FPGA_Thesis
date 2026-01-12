22e53a3808991f050fedc2faa14a94f08c77b7e9
aghidalgo04 <a.ghidalgo04@gmail.com>
2026-01-12 21:05:24 +0100

    added changes in wavelet_phase

--- ECG_in_FPGA_Thesis.srcs/sources_1/new/wavelet_phase.vhd
+++ ECG_in_FPGA_Thesis.srcs/sources_1/new/wavelet_phase.vhd
@@ -22,6 +22,46 @@ architecture Behavioral of wavelet_phase is
     signal decimate_toggle : std_logic := '0'; -- Para alternar (quedarse 1, tirar 1)
 
 begin
-    (r0, r1, r2, r3) <= (others => (others => '0')) WHEN clk = '1' AND CLK'EVENT AND reset = '1';
-    
+    process(clk)
+        -- Variables temporales para la matemática completa (evitar overflow antes de dividir)
+        variable sum_h : signed(26 downto 0); -- 3 bits extra para la suma
+        variable diff_g : signed(23 downto 0);
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                r0 <= (others => '0'); r1 <= (others => '0'); 
+                r2 <= (others => '0'); r3 <= (others => '0');
+                decimate_toggle <= '0';
+                d_out_valid <= '0';
+            else
+                d_out_valid <= '0'; -- Por defecto '0'
+
+                if d_in_valid = '1' then
+                    -- 1. Mover el Shift Register (Entra dato nuevo por r0)
+                    r3 <= r2;
+                    r2 <= r1;
+                    r1 <= r0;
+                    r0 <= d_in;
+
+                    -- 2. Control de Diezmado (Solo procesamos 1 de cada 2 muestras)
+                    decimate_toggle <= not decimate_toggle;
+
+                    if decimate_toggle = '1' then -- Muestra IMPAR (Procesamos)
+                        
+                        -- === FILTRO PASO BAJO (Spline: 1/8 * [1, 3, 3, 1]) ===
+                        -- Multiplicar por 3 es lo mismo que (x*2) + x. Es decir: (x << 1) + x
+                        sum_h := resize(r0, 27) + 
+                                 resize((r1 & '0') + r1, 27) + -- r1 * 3
+                                 resize((r2 & '0') + r2, 27) + -- r2 * 3
+                                 resize(r3, 27);
+                        
+                        -- Desplazar 3 bits a la derecha(/8)
+                        y <= sum_h(26 downto 3); 
+                        d <= r1 - r2;
+                        d_out_valid <= '1';
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
 end Behavioral;
