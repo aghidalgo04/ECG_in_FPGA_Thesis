72b816dbaf6cc173f7aa7547f6c3202564839d63
aghidalgo04 <a.ghidalgo04@gmail.com>
2026-01-12 21:14:40 +0100

    added changes in wavelet_phase

--- ECG_in_FPGA_Thesis.srcs/sources_1/new/wavelet_phase.vhd
+++ ECG_in_FPGA_Thesis.srcs/sources_1/new/wavelet_phase.vhd
@@ -6,7 +6,7 @@ entity wavelet_phase is
 Port (
         clk         : in  STD_LOGIC;
         reset       : in  STD_LOGIC;
-        -- Entrada
+        -- Entradas
         d_in_valid  : in  STD_LOGIC;
         d_in        : in  SIGNED(23 downto 0);
         -- Salidas
@@ -19,40 +19,40 @@ end wavelet_phase;
 architecture Behavioral of wavelet_phase is
     
     signal r0, r1, r2, r3 : signed(23 downto 0) := (others => '0');
-    signal decimate_toggle : std_logic := '0'; -- Para alternar (quedarse 1, tirar 1)
+    signal decimate_toggle : std_logic := '0';
 
 begin
     process(clk)
-        -- Variables temporales para la matemática completa (evitar overflow antes de dividir)
-        variable sum_h : signed(26 downto 0); -- 3 bits extra para la suma
+        variable sum_h : signed(26 downto 0); -- 3 bits mas para la suma
         variable diff_g : signed(23 downto 0);
     begin
         if rising_edge(clk) then
             if reset = '1' then
-                r0 <= (others => '0'); r1 <= (others => '0'); 
-                r2 <= (others => '0'); r3 <= (others => '0');
+                r0 <= (others => '0'); 
+                r1 <= (others => '0'); 
+                r2 <= (others => '0'); 
+                r3 <= (others => '0');
                 decimate_toggle <= '0';
                 d_out_valid <= '0';
             else
-                d_out_valid <= '0'; -- Por defecto '0'
+                d_out_valid <= '0';
 
                 if d_in_valid = '1' then
-                    -- 1. Mover el Shift Register (Entra dato nuevo por r0)
+                    -- Shift Register
                     r3 <= r2;
                     r2 <= r1;
                     r1 <= r0;
                     r0 <= d_in;
 
-                    -- 2. Control de Diezmado (Solo procesamos 1 de cada 2 muestras)
+                    -- Solo se procesan 1 de cada 2 muestras(Teorema de Nyquist)
                     decimate_toggle <= not decimate_toggle;
 
-                    if decimate_toggle = '1' then -- Muestra IMPAR (Procesamos)
+                    if decimate_toggle = '1' then 
                         
                         -- === FILTRO PASO BAJO (Spline: 1/8 * [1, 3, 3, 1]) ===
-                        -- Multiplicar por 3 es lo mismo que (x*2) + x. Es decir: (x << 1) + x
                         sum_h := resize(r0, 27) + 
-                                 resize((r1 & '0') + r1, 27) + -- r1 * 3
-                                 resize((r2 & '0') + r2, 27) + -- r2 * 3
+                                 (shift_left(resize(r1, 27), 1) + resize(r1, 27)) + -- r1 * 3
+                                 (shift_left(resize(r2, 27), 1) + resize(r2, 27)) + -- r2 * 3
                                  resize(r3, 27);
                         
                         -- Desplazar 3 bits a la derecha(/8)
