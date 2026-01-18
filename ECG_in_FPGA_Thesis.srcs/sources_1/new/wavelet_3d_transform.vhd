d0474b8b6ef576017689c558c1ac12427a382fc1
aghidalgo04 <a.ghidalgo04@gmail.com>
2026-01-18 15:38:35 +0100

    wavelet_3d_transform changes

--- ECG_in_FPGA_Thesis.srcs/sources_1/new/wavelet_3d_transform.vhd
+++ ECG_in_FPGA_Thesis.srcs/sources_1/new/wavelet_3d_transform.vhd
@@ -5,24 +5,25 @@ use IEEE.NUMERIC_STD.ALL;
 entity wavelet_3d_transform is
 Port (
         -- 1. CONTROL GENERAL
-        clk             : in  STD_LOGIC; -- Reloj de 100 MHz
+        clk             : in  STD_LOGIC;
         reset           : in  STD_LOGIC;
         
-        -- 2. ENTRADA DE DATOS (Vienen del módulo ADS1293)
-        sample_valid_in : in  STD_LOGIC; -- El pulso de "Dato Nuevo"
+        -- 2. ENTRADA DE DATOS (del módulo ADS1293)
+        sample_valid_in : in  STD_LOGIC;
         raw_x           : in  STD_LOGIC_VECTOR(23 downto 0);
         raw_y           : in  STD_LOGIC_VECTOR(23 downto 0);
         raw_z           : in  STD_LOGIC_VECTOR(23 downto 0);
 
-        -- 3. SALIDAS DE SEÑAL LIMPIA (Para visualizar/CORDIC)
-        -- La Wavelet elimina el ruido de alta frecuencia y la línea base
+        -- 3. SALIDAS DE SEÑAL LIMPIA
+        -- La Wavelet elimina el ruido de alta frecuencia
         wavelet_x         : out STD_LOGIC_VECTOR(23 downto 0);
         wavelet_y         : out STD_LOGIC_VECTOR(23 downto 0);
         wavelet_z         : out STD_LOGIC_VECTOR(23 downto 0);
         
         vector_ready    : out  STD_LOGIC;
-        d_vector          : out  SIGNED(23 downto 0);
-        y_vector         : out SIGNED(23 downto 0)
+        d_vector_s3     : out  SIGNED(23 downto 0);
+        y_vector_s3     : out SIGNED(23 downto 0);
+        d_vector_s8     : out SIGNED(23 downto 0)
     );
 end wavelet_3d_transform;
 
@@ -34,14 +35,16 @@ architecture Behavioral of wavelet_3d_transform is
             reset         : in  STD_LOGIC;
             sample_valid  : in  STD_LOGIC;
             raw_data      : in  STD_LOGIC_VECTOR(23 downto 0);
-            y_wavelet     : out SIGNED(23 downto 0);
-            d_wavelet     : out SIGNED(23 downto 0);
+            y_wavelet_s3     : out SIGNED(23 downto 0);
+            d_wavelet_s3     : out SIGNED(23 downto 0);
+            d_wavelet_s8     : out SIGNED(23 downto 0);
             wavelet_ready : out STD_LOGIC
         );
 end component;
     
-    signal yx, yy, yz : SIGNED(23 downto 0);
-    signal dx, dy, dz : SIGNED(23 downto 0);
+    signal yx3, yy3, yz3 : SIGNED(23 downto 0);
+    signal dx3, dy3, dz3 : SIGNED(23 downto 0);
+    signal dx8, dy8, dz8 : SIGNED(23 downto 0);
     signal rdyx, rdyy, rdyz : STD_LOGIC;
     signal abs_dx, abs_dy, abs_dz : signed(23 downto 0);
 
@@ -52,8 +55,9 @@ begin
         reset => reset,
         sample_valid => sample_valid_in,
         raw_data => raw_x,
-        y_wavelet => yx,
-        d_wavelet => dx,
+        y_wavelet_s3 => yx3,
+        d_wavelet_s3 => dx3,
+        d_wavelet_s8 => dx8,
         wavelet_ready => rdyx
     );
     
@@ -63,8 +67,9 @@ begin
         reset => reset,
         sample_valid => sample_valid_in,
         raw_data => raw_y,
-        y_wavelet => yy,
-        d_wavelet => dy,
+        y_wavelet_s3 => yy3,
+        d_wavelet_s3 => dy3,
+        d_wavelet_s8 => dy8,
         wavelet_ready => rdyy
     );
     
@@ -74,8 +79,9 @@ begin
         reset => reset,
         sample_valid => sample_valid_in,
         raw_data => raw_z,
-        y_wavelet => yz,
-        d_wavelet => dz,
+        y_wavelet_s3 => yz3,
+        d_wavelet_s3 => dz3,
+        d_wavelet_s8 => dz8,
         wavelet_ready => rdyz
     );
     
@@ -84,16 +90,20 @@ begin
         if rising_edge(clk) then
             if reset = '1' then
                 vector_ready <= '0';
-                d_vector <= (others => '0');
-                y_vector <= (others => '0');
+                d_vector_s3 <= (others => '0');
+                y_vector_s3 <= (others => '0');
             else
-                vector_ready <= '0'; -- Por defecto '0'
+                vector_ready <= '0';
 
                 if rdyz = '1' then
-                    -- Sumamos los absolutos (aseguramos no desbordamiento con resize si fuera necesario, 
-                    -- pero 24 bits suele sobrar para ECG)
-                    d_vector <= abs(dx) + abs(dy) + abs(dz);
-                    y_vector <= abs(yx) + abs(yy) + abs(yz);
+                    -- Sumamos los absolutos
+                    d_vector_s3 <= abs(dx3) + abs(dy3) + abs(dz3);
+                    d_vector_s8 <= abs(dx8) + abs(dy8) + abs(dz8);
+                    y_vector_s3 <= abs(yx3) + abs(yy3) + abs(yz3);
+                    
+                    wavelet_x <= std_logic_vector(yx3);
+                    wavelet_y <= std_logic_vector(yy3);
+                    wavelet_z <= std_logic_vector(yz3);
                     
                     vector_ready <= '1';
                 end if;
