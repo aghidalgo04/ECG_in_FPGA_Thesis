8b9f0e993396ad15e445244ff2b04c8426968df8
aghidalgo04 <a.ghidalgo04@gmail.com>
2026-01-23 02:10:36 +0100

    created new modules

---
+++ ECG_in_FPGA_Thesis.srcs/sources_1/new/heart_attack_detector.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 23.01.2026 02:06:00
+-- Design Name: 
+-- Module Name: heart_attack_detector - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity heart_attack_detector is
+--  Port ( );
+end heart_attack_detector;
+
+architecture Behavioral of heart_attack_detector is
+
+begin
+
+
+end Behavioral;
